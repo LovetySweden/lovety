80bedf1ef2df48eff8e26701a0574f4dca938da7
LovetySweden <info@singelaktiviteter.se>
2025-06-09 17:27:31 +0200

    Update GoogleSheetService.ts

--- src/services/GoogleSheetService.ts
+++ src/services/GoogleSheetService.ts
@@ -173,7 +173,7 @@ class GoogleSheetService {
         earlyBirdPrice: "250 kr",
         earlyBirdExpiryDate: "2025-06-18",
         externalPaymentLink: "https://example.com/payment/activity-4",
-        image: "https://images.unsplash.com/photo-1544367567-0f2fcb009e0b",
+        image: "/lovable-uploads/walk35.png",
         isFull: false,
         isOnSale: true
       },
