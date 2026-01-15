bec1d70dd39312016b6c77efd3ec9a684aa33ef9
LovetySweden <info@singelaktiviteter.se>
2026-01-15 13:22:04 +0100

    Update FunTogether.tsx

--- src/components/FunTogether.tsx
+++ src/components/FunTogether.tsx
@@ -3,19 +3,22 @@ const FunTogether = () => {
   return (
     <div className="bg-white py-12 px-4 md:px-8">
       <div className="container mx-auto">
-        <h2 className="section-title">Ha roligt tillsammans</h2>
+        <h2 className="section-title">Ett Sätt Att Träffas Och Ha Roligt Tillsammans</h2>
         <div className="max-w-4xl">
           <p className="text-lg mb-4">
             Trött på ytliga dating appar? Krystade dejter?
           </p>
           <p className="mb-6">
-            Lovety är en mötesplats där du kan vara dig själv, träffa andra singlar som delar dina intressen och lära känna varandra under avslappnade former.
+            Lovety är en mötesplats där du kan vara dig själv, träffa och lära känna andra singlar under avslappnade former.
           </p>
           <p className="mb-6">
             Det finns något för alla, både den som föredrar mer stillsamma event och den som älskar det fartfyllda!
           </p>
           <p className="mb-6">
             Våga utmana dig och träffa nya människor under roliga aktiviteter och event i Umeå!
+          </p>
+          <p className="mb-6">
+            PS - vi styr upp det, så du behöver inte oroa dig för att ta initiativ för samtal själv.
           </p>
         </div>
       </div>
